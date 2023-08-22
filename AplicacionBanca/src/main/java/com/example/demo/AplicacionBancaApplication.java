25cc29ea3cfe9003ea51d2dcdf445369ad0c7a4c
jmartsen <jmartsen@SVQ-F84M793.usersad.everis.int>
2023-08-22 14:36:24 +0200

    crear proyecto

---
+++ AplicacionBanca/src/main/java/com/example/demo/AplicacionBancaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionBancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionBancaApplication.class, args);
+	}
+
+}
