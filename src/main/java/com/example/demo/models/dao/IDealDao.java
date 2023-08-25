a9c01f6302b4398a71768ce348b8ac0c632b782d
Arthurcep <80314312+Arthurcep@users.noreply.github.com>
2023-08-25 09:14:09 +0200

    Revert "Hecho CRUD del Deal"
    This reverts commit 6c8198b2e1b1f11ff8b2eb3bc2860ef1cab73dba.

--- src/main/java/com/example/demo/models/dao/IDealDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.models.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.models.entity.DealEntity;
-
-public interface IDealDao extends JpaRepository<DealEntity, Integer> {
-
-}
